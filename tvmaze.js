a35844528abd7f9f54da96faa82041742892f2ac
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-02-28 17:56:14 -0800

    styling, display episodes on bootstrap modal

--- tvmaze.js
+++ tvmaze.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const $showsList = $("#showsList");
-const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $modalTitle = $('.modal-title');
+const $modalBody = $('.modal-body');
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -11,40 +12,36 @@ const $searchForm = $("#searchForm");
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
 async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
   let response = await axios.get(`http://api.tvmaze.com/search/shows?q= ${term}`);
-  let data = response.data;
-  let result = [];
-  for (let { show } of data) {
+  let shows = [];
+  for (let { show } of response.data) {
     let id = show.id;
     let name = show.name;
     let summary = show.summary;
     let image = show.image?.original;
-    result.push({ id, name, summary, image })
+    shows.push({ id, name, summary, image })
   }
-  return result;
+  return shows;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
-
 function populateShows(shows) {
   $showsList.empty();
 
   for (let show of shows) {
     const $show = $(
-      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" data-show-title="${show.name}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
               src="${show.image}" 
               alt="${show.name}" 
               class="w-25 mr-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
+             <h5 class="text-info">${show.name}</h5>
              <div><small>${show.summary}</small></div>
-             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+             <button class="btn btn-outline-light btn-sm Show-getEpisodes" data-bs-toggle="modal" data-bs-target="#modal">
                Episodes
              </button>
            </div>
@@ -60,12 +57,9 @@ function populateShows(shows) {
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchForShowAndDisplay() {
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
-
-  $episodesArea.hide();
   populateShows(shows);
 }
 
@@ -76,37 +70,39 @@ $searchForm.on("submit", async function (evt) {
 
 
 /** Given a show ID, get from API and return (promise) array of episodes:
- *      { id, name, season, number }
+ *      { id, name, season, number, summary }
  */
-
 async function getEpisodesOfShow(id) {
   let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
-  //check that data is empty array if no episodes
   return response.data.map((ep) => ({
     id: ep.id,
     name: ep.name,
     season: ep.season,
-    number: ep.number
+    number: ep.number,
+    summary: ep.summary
   }));
 }
 
-/** appends episode information to episode area in html */
-function populateEpisodes(episodes) {
-  let $episodesList = $('#episodesList');
-  $episodesList.empty();
+/** appends episode information to modal in html */
+function populateEpisodes(episodes, title) {
+  $modalBody.empty();
+  $modalTitle.text(`${title}`)
   for (let episode of episodes) {
-    let $item = $(`<li>
-      name ${episode.name}, season ${episode.season}, number ${episode.number} 
-    </li>`);
-    $episodesList.append($item);
+    const $item = $(
+      `<div class="mb-4">
+        <strong class="text-info">
+          S${episode.season} E${episode.number} : <em>${episode.name}</em>
+        </strong>
+        ${episode.summary}
+      </div>`);
+    $modalBody.append($item);
   }
-  $episodesArea.show();
 }
 
 $showsList.on('click', '.btn', async function (evt) {
-  let id = $(evt.target).closest(".Show").data("show-id");
+  const $showDiv = $(evt.target).closest(".Show");
+  let id = $showDiv.data("show-id");
+  let title = $showDiv.data("show-title");
   let episodes = await getEpisodesOfShow(id);
-  populateEpisodes(episodes);
+  populateEpisodes(episodes, title);
 })
-
-
